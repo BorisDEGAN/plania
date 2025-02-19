a0afb6a6f529d41f2b317827d6a2756c28cd952d
skywalkerx64 <rickjoaodonald@gmail.com>
2025-02-19 09:11:24 +0100

    wip

--- src/components/Header/DropdownUser.tsx
+++ src/components/Header/DropdownUser.tsx
@@ -66,7 +66,7 @@ const DropdownUser = () => {
       >
         <span className="hidden text-right lg:block">
           <span className="block text-sm font-medium text-black dark:text-white">
-            {user?.fullname}
+            {user?.fullname}.
           </span>
         </span>
 
